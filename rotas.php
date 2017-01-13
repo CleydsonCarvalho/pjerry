ced85c0de9c4dd7e216c00d732f96903d6dfea99
JhonatasOliveira <jhonneo33@hotmail.com>
2017-01-13 06:16:56 -0300

    rotas

--- rotas.php
+++ rotas.php
@@ -1,32 +1,64 @@
+<?php
+
+function __autoload( $class_name ) {
+	require_once 'classes/' . $class_name . '.php';
+}
+
+$rota = new Rota();
+
+if ( isset( $_POST[ 'acao' ] ) && $_POST[ 'acao' ] == 'cadastrar' ) {
+
+	$remover = array(
+		$_POST[ 'acao' ],
+	);
+
+	$resultado = array_diff( $_POST, $remover );
+	$criar = array_filter( $resultado );
+	$nColunas = count( $criar );
+
+	if ( $nColunas > $nColunas ) {
+		$erro = 'sim';
+	} else {
+		$rota->create( $criar );
+	}
+}
+
+if ( isset( $_POST[ 'acao' ] ) && $_POST[ 'acao' ] == 'atualizar' ) {
+
+	$remover = array(
+		$_POST[ 'acao' ],
+	);
+
+	$resultado = array_diff( $_POST, $remover );
+	$atualizar = array_filter( $resultado );
+
+	$rota->update( $atualizar );
+	 header("Location: carros.php");
+}
+
+if ( isset( $_GET[ 'acao' ] ) && $_GET[ 'acao' ] == 'excluir' ) {
+	$rota->delete( $id = $_GET[ 'id' ] );
+}
+?>
 <!doctype html>
 <html>
 <meta lang="pt-br">
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
 <meta name="description" content="">
 <meta name="author" content="">
 <link rel="icon" href="#">
-<title>S.G.V</title>
+<title>Sistema de Vendas</title>
 
-<!-- Bootstrap core CSS -->
 <link rel="stylesheet" href="css/bootstrap.min.css">
 <link rel="stylesheet" href="css/font-awesome.min.css">
 <link rel="stylesheet" href="css/menu.css">
 <link rel="stylesheet" href="css/layout-pag-rotas.css">
 <link rel="stylesheet" href="css/ajustes-font.css">
-<!-- jQuery (obrigatório para plugins JavaScript do Bootstrap) -->
 <script src="js/jquery.min.js"></script>
 <script type="text/javascript" src="js/jquery.quick.search.js"></script>
-
-<script>
-	$( '#visualizarModal' ).on( 'shown.bs.modal', function () {
-		$( '#myInput' ).focus()
-	} )
-	
-</script>
-
 </head>
 
 <body>
@@ -44,27 +76,25 @@
 	<div class="container">
 		<div class="row">
 
-
-
 			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
 
 				<h1>Rotas</h1>
 				<p>Cadastro de rotas.</p>
 				<hr>
-				<form>
+				<form method="post" enctype="multipart/form-data" >
 
 					<div class="row">
 						<div class="col-xs-12 col-sm-12 col-md-9 col-lg-9 app-margimBotomCamposFomr">
 							<label for="nome"><spam class="app-astericoRed">*</spam> Nome da Rota:</label>
-							<input type="text" class="form-control" id="nome" placeholder="Nome" required>
+							<input type="text" class="form-control" name="nome" id="nome" placeholder="Nome" required>
 						</div>
 
 						<div class="col-xs-12 col-sm-2 col-md-3 col-lg-3 app-margimBotomCamposFomr">
-							<label for="numero"><spam class="app-astericoRed">*</spam> Estado:</label>
-							<select class="form-control">
+							<label for="estado"><spam class="app-astericoRed">*</spam> Estado:</label>
+							<select name="estado" id="estado" class="form-control" required>
 								<option value="">Selecione o Estado</option>
-								<option value="piaui">Piaui</option>
-								<option value="maranhao">Maranhão</option>
+								<option >Piaui</option>
+								<option >Maranhão</option>
 							</select>
 						</div>
 						<div>
@@ -74,64 +104,69 @@
 					</div>
 
 
-					<button type="submit" class="btn btn-primary btn-lg pull-right">Gravar Dados</button>
+					<button type="submit" name="acao" value="cadastrar" class="btn btn-primary btn-lg pull-right">Gravar Dados</button>
 				</form><br><br><br><br>
 
+				<!--Tabela de Clientes-->
 				<h1 class="zerarMargimH">Rotas Cadastradas</h1>
 				<p>Lista com todas as rotas.</p>
 				<hr>
 				<div class="form-group">
 					<input type="text" class="form-control input-search" alt="lista-clientes" placeholder="Buscar na Lista">
 				</div>
+			
 				<div class="table-responsive">
-
-					<table class="table table-hover table-striped lista-clientes" id="tabela">
+					<table class="table table-hover lista-clientes" id="tabela">
 						<thead>
 							<tr>
-								<th>Código</th>
-								<th>Nome da Rota</th>
+								<th>N°</th>
+								<th>Rota</th>
 								<th>Estado</th>
 								<th class="app-btn-acoes text-center">Ações</th>
 							</tr>
 						</thead>
 						<tbody>
-
 							<tr>
-								<td scope="row">79</td>
-								<td>Espelho 2M</td>
-								<td>2</td>
+								<?php $n=0;
+								foreach($rota->readAll() as $key => $value_rota){ $n++;?>
+								
+								<td>
+									<?=$n?>
+								</td>
+								<td>
+									<?=$value_rota->nome?>
+								</td>
+								<td>
+									<?=$value_rota->estado?>
+								</td>
 
-								<td class="app-btn-acoes text-center">
-									<button type="button" class="btn btn-success" data-toggle="modal" data-target="#visualizarModal">Visulaizar</button>
-									<button type="button" class="btn btn-warning" >Editar</button>
-									<button type="button" class="btn btn-danger">Excluir</button>
+								<td class="app-btn-acoes">
+
+									<button type="button" 
+									data-nome="<?= $value_rota->id ?>" 
+									data-rg="<?= $value_rota->nome ?>" 
+									data-cpf="<?= $value_rota->estado ?>" 
+									
+									
+									class="visualizar btn btn-success">Visualizar</button>
+
+									<button type="button" 
+										data-nome="<?= $value_rota->id ?>" 
+									data-rg="<?= $value_rota->nome ?>" 
+									data-cpf="<?= $value_rota->estado ?>" 
+									
+									class="editar btn btn-warning">Editar</button>
+									
+									<button type="button" 
+										data-nome="<?= $value_rota->id ?>" 
+									data-rg="<?= $value_rota->nome ?>" 
+									data-cpf="<?= $value_rota->estado ?>" 
+									
+									class="excluir btn btn-danger">Excluir</button>
+
 								</td>
 							</tr>
-							<tr>
-								<td scope="row">79</td>
-								<td>Espelho 2M</td>
-								<td>2</td>
-
-								<td class="app-btn-acoes text-center">
-									<button type="button" class="btn btn-success" data-toggle="modal" data-target="#visualizarModal">Visulaizar</button>
-									<button type="button" class="btn btn-warning" >Editar</button>
-									<button type="button" class="btn btn-danger">Excluir</button>
-								</td>
-							</tr>
-							<tr>
-								<td scope="row">79</td>
-								<td>Espelho 2M</td>
-								<td>2</td>
-
-								<td class="app-btn-acoes text-center">
-									<button type="button" class="btn btn-success" data-toggle="modal" data-target="#visualizarModal">Visulaizar</button>
-									<button type="button" class="btn btn-warning" >Editar</button>
-									<button type="button" class="btn btn-danger">Excluir</button>
-								</td>
-							</tr>
-
-
-							
+							<?php } ?>
 						</tbody>
 					</table>
 				</div>
